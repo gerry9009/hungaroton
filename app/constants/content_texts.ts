8059ed06bb67601f60a19c95cf04d8ddeb7dccfe
Gergo Buglyo <buglyogergo@gmail.com>
2025-10-04 00:09:51 +0200

    feat: add text constants

---
+++ app/constants/content_texts.ts
@@ -0,0 +1,27 @@
+export const CONTENT_TEXTS = {
+  home: {
+    welcome: "Üdv a Hungaroton katalógus Oldalon",
+    welcomeMessage: "Böngészd a művészeket és fedezd fel a zenéiket!",
+    button: "Fedezd fel az Előadókat",
+  },
+  artists: {
+    title: "Hungaroton Artist",
+    footer: "Fotexnet",
+    button: "Részletek",
+    albums: "Albumok: {count}",
+    form: {
+      searchLabel: "Keresés",
+      typeLabel: "Típus",
+      label_composer: "Szerző",
+      label_performer: "Előadó",
+      label_primary: "Elsődleges előadó",
+      letterLabel: "Kezdő betű",
+      typeEmptyOption: "Nincs kiválasztva",
+    },
+  },
+  artistDetails: {
+    noAlbums: "Nincs elérhető album",
+    albums: "Összesen: {albums} album",
+    backButton: "Vissza az Előadókhoz",
+  },
+};
